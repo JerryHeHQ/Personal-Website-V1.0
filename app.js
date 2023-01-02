2476bc182ad9cbd702dc7832b1f4b39c76d36857
ExoHorizon <github@exohorizon.tech>
2023-01-01 22:21:39 -0600

    Reset Design ⬢ Removed background parallax ⬢ Removed all:     ⬢ Different font sizes, weights, etc.     ⬢ Margins/Borders/Padding     ⬢ Max-Widths ⬢ Code Cleanup

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
 // Parallax Section Start
-document.addEventListener('DOMContentLoaded', function () {
-    backgroundParallax();
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//     backgroundParallax();
+// });
 
-window.addEventListener('scroll', function () {
-    backgroundParallax();
-});
+// window.addEventListener('scroll', function () {
+//     backgroundParallax();
+// });
 
-window.addEventListener('resize', function () {
-    backgroundParallax();
-});
+// window.addEventListener('resize', function () {
+//     backgroundParallax();
+// });
 
-function backgroundParallax() {
-    var backgroundImage = new Image();
-    backgroundImage.src = 'img/Background.png';
-    var backgroundAspectRatio = backgroundImage.height / backgroundImage.width;
+// function backgroundParallax() {
+//     var backgroundImage = new Image();
+//     backgroundImage.src = 'img/Background.png';
+//     var backgroundAspectRatio = backgroundImage.height / backgroundImage.width;
 
-    var visibleAspectRatio = document.documentElement.clientHeight / document.documentElement.clientWidth;
-    var hiddenHeight = getHiddenHeight(backgroundAspectRatio, visibleAspectRatio);
+//     var visibleAspectRatio = document.documentElement.clientHeight / document.documentElement.clientWidth;
+//     var hiddenHeight = getHiddenHeight(backgroundAspectRatio, visibleAspectRatio);
 
-    var scrollPercent = getScrollPercent();
-    var yOffset = -0.01 * scrollPercent * hiddenHeight;
-    document.querySelector('.parallax-background').style.backgroundPosition = 'center ' + yOffset + 'px';
-}
+//     var scrollPercent = getScrollPercent();
+//     var yOffset = -0.01 * scrollPercent * hiddenHeight;
+//     document.querySelector('.parallax-background').style.backgroundPosition = 'center ' + yOffset + 'px';
+// }
 
-function getScrollPercent() {
-    var docElement = document.documentElement
-    var docBody = document.body;
-    var scrollTop = docElement['scrollTop'] || docBody['scrollTop'];
-    var scrollHeight = docElement['scrollHeight'] || docBody['scrollHeight'];
-    return (scrollTop) / ((scrollHeight) - docElement.clientHeight) * 100;
-}
+// function getScrollPercent() {
+//     var docElement = document.documentElement
+//     var docBody = document.body;
+//     var scrollTop = docElement['scrollTop'] || docBody['scrollTop'];
+//     var scrollHeight = docElement['scrollHeight'] || docBody['scrollHeight'];
+//     return (scrollTop) / ((scrollHeight) - docElement.clientHeight) * 100;
+// }
 
-function getHiddenHeight(backgroundAspectRatio, visibleAspectRatio) {
-    if (visibleAspectRatio >= backgroundAspectRatio) {
-        return 0;
-    }
-    visibleWidth = document.documentElement.clientWidth;
-    visibleHeight = document.documentElement.clientHeight;
-    return backgroundAspectRatio * visibleWidth - visibleHeight;
-}
+// function getHiddenHeight(backgroundAspectRatio, visibleAspectRatio) {
+//     if (visibleAspectRatio >= backgroundAspectRatio) {
+//         return 0;
+//     }
+//     visibleWidth = document.documentElement.clientWidth;
+//     visibleHeight = document.documentElement.clientHeight;
+//     return backgroundAspectRatio * visibleWidth - visibleHeight;
+// }
 // Parallax Section End
 
 // Skills Section Scroll Animation Start
